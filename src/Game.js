c9b7abb03efe2471d7e5b44de28d153971f9dcad
Herman <herman.vesterlund@elev.ga.ntig.se>
2024-11-04 11:11:48 +0100

    adasdas

---
+++ src/Game.js
@@ -0,0 +1,18 @@
+import GameObject from "./GameObject"
+
+export default class game {     //skapar klassen
+    constructor(width, height) {    //klassens konstruktor
+        this.width = width
+        this.height = height
+        console.log("Ny instant av game", this.width)
+        this.box = new GameObject (40, 100, 80, 80, "green")
+    }
+
+    update(deltaTime){
+        this.box.update(deltaTime)
+    }
+
+    draw(ctx){
+        this.box.draw(ctx)
+    }
+}
